7493e7652fbd8900ef1d69fcec5f6769a11acc4e
lorenzgithub <52162521+lorenzgithub@users.noreply.github.com>
2020-04-23 21:28:07 +0200

    Solving an Error. One Line was not deleted from the merge

--- R/generateSierpinski.R
+++ R/generateSierpinski.R
@@ -22,7 +22,6 @@ sampleVertex <- function(n) {
   angle = 2 * pi / n * (sample(n, 1))
 
   midpoint + radius * c(sin(angle), cos(angle))
-  c(x=1, y=1)
 }
 
 # Create a point that is a step from `from` to `to`
